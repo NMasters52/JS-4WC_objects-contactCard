b96b47c10d8feb029ccd293ce7b3c2b290b8062e
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-03-05 16:51:35 +0000

    Added methods to grab all information from the object

--- script.js
+++ script.js
@@ -13,6 +13,21 @@ const contactCard = {
     //method to grab the full name
     getFullName() {
         return `${this.firstName} ${this.lastName}`;
-    }
+    },
+
+    //method to grab the address
+    getAddress() {
+        return `${this.address.street}
+        ${this.address.city}, ${this.address.state} ${this.address.zipCode}`;
+    },
+
+    //method to grab contact information
+    getContactInformation() {
+        return `${this.phoneNumber}
+                ${this.email}`;
+    },
+
+    //have the information appear on the screen
+    document.getElementById('')
 }
 
