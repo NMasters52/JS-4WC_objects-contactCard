f047a1c286d720fbaf3162da507884dad057fd27
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-03-26 16:45:49 +0000

    moved contactCard object to seperate file. Imported the file as a module to script JS

---
+++ contact.js
@@ -0,0 +1,32 @@
+const contactCard = {
+    firstName : "Nick",
+    lastName : "Masters",
+    phoneNumber : "(904)123-4567",
+    email : "example@example.com",
+    address : {
+        street : "123 Avandale st",
+        city : "imaginary",
+        state : "Florida",
+        zipCode : "12345"
+    },
+
+    //method to grab the full name
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    },
+
+    //method to grab the address
+    getAddress() {
+        return `${this.address.street}
+        ${this.address.city}, ${this.address.state} ${this.address.zipCode}`;
+    },
+
+    //method to grab contact information
+    getContactInformation() {
+        return `${this.phoneNumber}
+                ${this.email}`;
+    },
+
+};
+
+export default contactCard;
--- script.js
+++ script.js
@@ -1,33 +1,4 @@
-const contactCard = {
-    firstName : "Nick",
-    lastName : "Masters",
-    phoneNumber : "(904)123-4567",
-    email : "example@example.com",
-    address : {
-        street : "123 Avandale st",
-        city : "imaginary",
-        state : "Florida",
-        zipCode : "12345"
-    },
-
-    //method to grab the full name
-    getFullName() {
-        return `${this.firstName} ${this.lastName}`;
-    },
-
-    //method to grab the address
-    getAddress() {
-        return `${this.address.street}
-        ${this.address.city}, ${this.address.state} ${this.address.zipCode}`;
-    },
-
-    //method to grab contact information
-    getContactInformation() {
-        return `${this.phoneNumber}
-                ${this.email}`;
-    },
-
-}
+import contactCard from './contact.js'
 
 //display information on the screen by targeting the nodes then using the objects methods to display the information
 document.getElementById('contactName').textContent = contactCard.getFullName();
